97eb52b88d6313143afad5fb647f8f6b64378ed6
KinKam Zhao <kamzh88@hotmail.com>
2020-02-23 01:53:17 -0500

    save new user to database

--- routes/api/manager.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const firebaseController = require("../../controllers/managerController");
-
-router.route("/")
-    .create(firebaseController.create)
-
-module.exports = router;
