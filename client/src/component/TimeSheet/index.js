2a563153a832a4fb8ae99b3ad1bd66a630a64ffd
KinKam Zhao <kamzh88@hotmail.com>
2020-02-29 11:34:02 -0500

    append employees shift

--- client/src/component/TimeSheet/index.js
+++ client/src/component/TimeSheet/index.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import Moment from 'react-moment';
 
-const TimeSheet = (props) => {
-    console.log(props.data)
-    return (
-        <div>Front page before signing in</div>
-    )
+class TimeSheet extends Component {
+
+    render() {
+
+        return (
+            <Fragment>
+                <div>{this.props.data.map((shifts, index) => (
+                    <div key={index}>
+                        <p>
+                            Date: {
+                                <Moment format="MM/DD/YYYY">
+                                    {shifts.StartTime}
+                                </Moment>
+                            }
+                        </p>
+                        <p>
+                            Start Shift: {
+                                <Moment format="h:mm:ss a">
+                                    {shifts.StartTime}
+                                </Moment>
+                                
+                            }
+                        </p>
+                        <p>
+                            Shift End: {
+                                <Moment format="h:mm:ss a">
+                                    {shifts.EndTime}
+                                </Moment>
+                            }
+                        </p>
+                        <br></br>
+                    </div>
+                ))}
+                </div>
+            </Fragment>
+        )
+    }
 }
 
 export default TimeSheet;
