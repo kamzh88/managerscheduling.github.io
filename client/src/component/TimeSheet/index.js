504fb1b1350386aa38322531e573daf7b6c07980
KinKam Zhao <kamzh88@hotmail.com>
2020-02-29 22:26:24 -0500

    add form into modal

--- client/src/component/TimeSheet/index.js
+++ client/src/component/TimeSheet/index.js
@@ -5,13 +5,17 @@ import ModalBody from "react-bootstrap/ModalBody";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
+import { Typography, TextField, Button } from '@material-ui/core';
 
 class TimeSheet extends Component {
 
     state = {
         isOpen: false,
         setIsOpen: false,
-        results: []
+        results: [],
+        date: '',
+        shiftStart: '',
+        shiftEnd: ''
     }
 
     showModal = (event) => {
@@ -22,12 +26,18 @@ class TimeSheet extends Component {
         this.setState({ isOpen: false, setIsOpen: false })
     };
 
+    handleChange = key => e => {
+        this.setState({ [key]: e.target.value });
+    };
+
     render() {
-        console.log(this.state.results)
+        const { date, shiftStart, shiftEnd } = this.state;
+        // console.log(this.state.results)
         return (
             <Fragment>
                 {this.props.data.map((shifts, index) => (
                     <button
+                        starttime={shifts.StartTime}
                         key={index}
                         onClick={() => this.showModal({
                             _id: shifts._id,
@@ -70,8 +80,64 @@ class TimeSheet extends Component {
                     <ModalHeader>
                         <ModalTitle>Edit Shifts</ModalTitle>
                     </ModalHeader>
-                    <ModalBody>{}
-                            
+                    <ModalBody>
+                        <form
+                            style={{ display: "flex", flexDirection: "column" }}
+                            onSubmit={this.onSubmit}
+                        >
+                            <TextField
+                                required
+                                style={{ marginBottom: 20 }}
+                                id="date"
+                                label="Select Date"
+                                type="date"
+                                defaultValue={date ? date : "2020-02-24"}
+                                onChange={this.handleChange("date")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
+                            <TextField
+                                required
+                                style={{ marginBottom: 20 }}
+                                id="shiftStart"
+                                label="Start Time"
+                                type="time"
+                                defaultValue={shiftStart ? shiftStart : "11:00"}
+                                onChange={this.handleChange("shiftStart")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                inputProps={{
+                                    step: 1800, // 5 min
+                                }}
+                            />
+                            <TextField
+                                required
+                                style={{ marginBottom: 20 }}
+                                id="shiftEnd"
+                                label="End Time"
+                                type="time"
+                                defaultValue={shiftEnd ? shiftEnd : "23:00"}
+                                onChange={this.handleChange("shiftEnd")}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                inputProps={{
+                                    step: 1800, // 5 min
+                                }}
+                            />
+                            <Button
+                                style={{ marginBottom: 20 }}
+                                type={"submit"}
+                                fullWidth
+                                variant={"contained"}
+                                color={"primary"}
+                            // disabled={isInvalid}
+                            >
+                                Submit
+                            </Button>
+                        </form>
                     </ModalBody>
                     <ModalFooter>This is the footer</ModalFooter>
                 </Modal>
