f844ed529a1e9a14b9c9486af77251aaa4fc5c50
KinKam Zhao <kamzh88@hotmail.com>
2020-03-01 20:42:57 -0500

    updates

---
+++ client/build/precache-manifest.894ecef89ebfca39f3713c111d833d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c776dd7992042aed369e08993f56fed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865e1d487d4783af28c1",
+    "url": "/static/css/main.a8b8f8b2.chunk.css"
+  },
+  {
+    "revision": "cb96ce171c521f80950b",
+    "url": "/static/js/2.39054bbe.chunk.js"
+  },
+  {
+    "revision": "11584c9dc2bb9186a9bdc598176d7a30",
+    "url": "/static/js/2.39054bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865e1d487d4783af28c1",
+    "url": "/static/js/main.56d986cf.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/manager_scheduling_app", { useFindAndModify: false });
 
 // Start the API server
